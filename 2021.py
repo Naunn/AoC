96205e768d9ed80edc8552058ed0c05300d7ba35
Bartosz Michał Lewandowski <Bartosz.Michal.Lewandowski@neuca.pl>
2022-06-03 17:59:23 +0200

    Rozwiazano zadanie 4 i dodano komentarze

--- 2021.py
+++ 2021.py
@@ -10,6 +10,7 @@ S = "53616c7465645f5f14044fb9e9c8529fb52cd27efaee89e40ccc4a65b109fd6eef451527404
 import pandas as pd
 import numpy as np
 from statistics import mode
+import copy
 # %% zadanie 1
 day_one = get_data(session=S, day=1, year=2021)
 
@@ -19,6 +20,7 @@ measurement = []
 for M in day_one.split('\n'):
     measurement.append(int(M))
     
+# Zliczanie increase i decrease
 def incr_decr(measures: list):
     increase = 0
     decrease = 0
@@ -42,9 +44,11 @@ incr_decr(measurement_3)
 # %% zadanie 2
 day_two = get_data(session=S, day=2, year=2021)
 
+# Slownik do zapisu polozenia
 position = {"horizontal position": 0,
             "depth": 0}
 
+# Obliczanie polozenia przez zmiany zapisow w slowniku
 for M in day_two.split('\n'):
     temp = M.split(" ")
     if temp[0] == "forward":
@@ -55,10 +59,12 @@ for M in day_two.split('\n'):
         position["depth"] -= int(temp[1])
 print(position["horizontal position"]*position["depth"])
 
+# Slownik rozszerzony o aim
 position = {"horizontal position": 0,
             "depth": 0,
             "aim": 0}
 
+# Obliczanie polozenia przez zmiany zapisow w slowniku z uwzglednieniem aim
 for M in day_two.split('\n'):
     temp = M.split(" ")
     if temp[0] == "forward":
@@ -75,8 +81,10 @@ day_three = get_data(session=S, day=3, year=2021)
 
 df = pd.DataFrame([list(M) for M in day_three.split("\n")])
 
+# Wykorzystanie desc() do wyciagniecia najczesciej wystepujacych wartosci
 top = df.describe().loc[["top"]].values[0]
 
+# Sklejanie wartosci z listy top w jeden ciag znakow
 gamma = ''
 epsilon = ''
 for _ in top:
@@ -87,8 +95,10 @@ for _ in top:
         gamma += '0'
         epsilon += '1'
 
+#  int(string, 2) oznacza przekonwertowanie stringu zero-jedynek w systemie binarnym (2) na int
 print("Power consumption of the submarine:",int(gamma, 2)*int(epsilon, 2))
 
+# Rekurencyjne odfiltrowywanie kolejnych tablic na dwa sposoby
 def rec(df, col: int, criteria: str):
     if criteria == "oxygen":
         if df.shape[0] > 1:
@@ -119,16 +129,123 @@ def rec(df, col: int, criteria: str):
     else:
         print("Wrong criteria!")
 
+# Funkcja sklejajaca elementy listy w jeden ciag znakow
 def glue(arr: list):
     glued = ""
     for _ in arr:
         glued += _
     return glued
 
+#  int(string, 2) oznacza przekonwertowanie stringu zero-jedynek w systemie binarnym (2) na int
 oxygen = int(glue(rec(df, 0, "oxygen").values[0]), 2)
 CO2 = int(glue(rec(df, 0, "CO2").values[0]), 2)
 
 print("The life support rating:",oxygen*CO2)
+
+# %% zadanie 4
+day_four = get_data(session=S, day=4, year=2021)
+
+drawn_numbers = [int(drawn) for drawn in day_four.split("\n\n")[0].split(",")]
+boards = day_four.split("\n\n")[1:]
+
+# Umieszczenie wszystkich elementow w jednej, dlugiej, tablicy i konwersja na tuplesy
+long_tab = []
+for board in boards:
+    for row in board.split("\n"):
+        for el in row.split():
+            long_tab.append((int(el),0))
+
+# znestowanie long_tab w liste list po 5 elementow
+nested_tab = [long_tab[x:x+5] for x in range(0, len(long_tab), 5)]
+
+# Sprawdzenie, czy wystepuje rzad lub wiersz jedynek dla zadanych indeksow
+def check(dictionary: dict, key, index: int):
+    col0 = []
+    col1 = []
+    # wyciagamy wagi dla wiersza przy wskazanym indeksie wiersza
+    row = [_[1] for _ in dictionary[key]]
+    # wyciagamy wagi i wartosci dla kolumny przy wskazanym indeksie kolumny
+    for i in range(len(dictionary.keys())):
+        col1.append(dictionary[i][index][1])
+        col0.append(dictionary[i][index][0])
+    if 0 not in col1:
+        return 1, col0
+    if 0 not in row:
+        return 1, [_[0] for _ in dictionary[key]]
+    else:
+        return [0]
+
+# Sprawdzenie tablicy/boardu po ilu krokach wystepuje bingo (jezeli wgl wystepuje)
+def board_check(board: dict, drawns: list):
+    copy_board = copy.deepcopy(board)
+    # iterujemy po wylosowanych numerach
+    for drawn in drawns:
+        # sprawdzamy schodzac kolejno po wierszach
+        for key in copy_board.keys():
+            # sprawdzamy kolejne elementy w wierszu
+            for (item, sign) in copy_board[key]:
+                # jezeli niesprawdzony to znakujemy go 1-ka
+                if item == drawn and sign == 0:
+                    index = copy_board[key].index((item, sign))
+                    copy_board[key][index] = (item, 1)
+                    # sprawdzamy czy po tej zmianie, nie pojawila sie wygrana
+                    temp = check(copy_board, key, index)
+                    if temp[0] > 0:
+                        return copy_board,temp[1],drawn,drawns.index(drawn)+1
+
+# sprawdzenie kolejnych tablic
+def find_board(numbers: list, long: list, nested: list, target: str):
+    win = len(numbers)
+    lost = 0
+    for i in range(0,int(len(long)/5),5):
+        temp_dict = dict(zip([_ for _ in range(0,5)],nested[i:i+5]))
+        t = board_check(temp_dict, numbers)[3]
+        if t <= win and target == "win":
+            board,win_numb,last_drawn,t1 = board_check(temp_dict, numbers)
+            win = t
+        if t >= lost and target == "lost":
+            board,win_numb,last_drawn,t1 = board_check(temp_dict, numbers)
+            lost = t
+    return board,win_numb,last_drawn,t1
+
+# zsumowanie wyrazow niezaznaczonych
+def game(numbers: list, long: list, nested: list, target: str):
+    board,win_numb,last_drawn,t1 = find_board(numbers, long, nested, target)
+    unmarked_sum = 0
+    for rows in [_ for _ in board.values()]:
+        for val in rows:
+            if val[1] == 0:
+                unmarked_sum += val[0]
+                
+    print("Final score is:",unmarked_sum*last_drawn)
+
+game(drawn_numbers, long_tab, nested_tab, "win")
+game(drawn_numbers, long_tab, nested_tab, "lost")
+
+# %% zadanie 5
+day_five = get_data(session=S, day=5, year=2021)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
