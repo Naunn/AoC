281eca3fd38f306e4e35e365f4b2b46f2793a338
Naunn <bartosz.m.lewandowski@gmail.com>
2023-03-05 23:00:22 +0100

    zad 12 cz. 2

--- 2021/12.py
+++ 2021/12.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 import yaml
+from collections import Counter
 
 
 with open("config.yaml", "r") as f:
@@ -110,5 +111,91 @@ all_paths(make_graph(tst3), "start", "end")  # 226
 print(
     "There is {} paths through this cave system where we visit small caves at most once.".format(
         all_paths(make_graph(day_twelve), "start", "end")
+    )
+)
+
+
+# def remove_bigger(x: list):
+#     temp = x[1:-1]
+#     for _ in temp:
+#         if _.isupper():
+#             temp.remove(_)
+#     return temp
+
+
+# def DFS_2(graph: dict, vertex: str, destination: str, visited: list, path: list) -> int:
+#     if vertex.islower():
+#         visited[vertex] += 1
+#     path.append(vertex)
+
+#     count = 0
+
+#     #temp = remove_bigger(path)
+#     if vertex == destination and (
+#         len(temp) - 1 == len(set(temp)) or len(temp) == len(set(temp))
+#     ):
+#         count = 1
+#     else:
+#         for v in graph[vertex]:
+#             if visited[v] < 2:  # and 2 not in list(visited.values())[1:]:
+#                 count += DFS_2(graph, v, destination, visited, path)
+
+#     path.pop()
+#     visited[vertex] -= 1
+
+#     return count
+
+
+# def all_paths_2(graph: dict, start: str, finish: str) -> int:
+#     visited = dict.fromkeys(graph.keys(), 0)
+#     visited[start] = 2
+#     visited[finish] = 1
+#     path = []
+#     return DFS_2(graph, start, finish, visited, path)
+
+
+def DFS_2(graph: dict, vertex: str, destination: str, visited: list, path: list) -> int:
+    if vertex.islower():
+        visited[vertex] += 1
+    path.append(vertex)
+
+    count = 0
+
+    if vertex == destination:
+        count = 1
+    else:
+        small_caves = len(
+            [
+                item
+                for item, count in Counter([i for i in path if i.islower()]).items()
+                if count > 1
+            ]
+        )
+        for v in graph[vertex]:
+            if (visited[v] == 0) or (
+                visited[v] < 2 and (small_caves == 0 or v.isupper())
+            ):
+                count += DFS_2(graph, v, destination, visited, path)
+
+    path.pop()
+    visited[vertex] -= 1
+
+    return count
+
+
+def all_paths_2(graph: dict, start: str, finish: str) -> int:
+    visited = dict.fromkeys(graph.keys(), 0)
+    path = []
+    visited[start] = 2
+    return DFS_2(graph, start, finish, visited, path)
+
+
+all_paths_2(make_graph(tst1), "start", "end")  # 36
+all_paths_2(make_graph(tst2), "start", "end")  # 103
+all_paths_2(make_graph(tst3), "start", "end")  # 3509
+
+print(
+    "There is {} paths through this cave system where we visit single small caves at most twice and other small caves at most once.".format(
+        all_paths_2(make_graph(day_twelve), "start", "end")
     )
 )
